2da7e0912f0cc9520b0dde03e44ca1101c8179a5
carlos <carlos@localhost>
2020-01-10 21:29:55 +0100

    Segundo Commit Eclipse (JavaDoc)

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -1,7 +1,14 @@
 package cuentas;
-
+/**
+ * Esta clase simula una cuenta de banco en la que se pueden ver los datos del titular, 
+ * tipo de interés y realizar  operaciones de ingreso y reintegro.
+ * 
+ * @author Carlos Royuela
+ * @version 1.0
+ * @since 09/01/2020
+ * 
+ */
 public class CCuenta {
-
 
     private String nombre;
     private String cuenta;
@@ -11,19 +18,32 @@ public class CCuenta {
     public CCuenta()
     {
     }
-
+    
+    /**
+     * @param nom Titular de la cuenta.
+     * @param cue Número de cuenta.
+     * @param sal Saldo disponible.
+     * @param tipo Tipo de interés.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
     }
-
+    
+    /**
+     * @return Devuelve el saldo disponible.
+     */
     public double estado()
     {
         return getSaldo();
     }
-
+    
+    /**
+     * @param cantidad Cantidad a ingresar en la cuenta.
+     * @throws Exception Error cuando se ingresa cantidad negativa.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -31,6 +51,10 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * @param cantidad Cantidad a retirar de la cuenta.
+     * @throws Exception Error cuando se retira cantidad negativa o no hay saldo suficuente.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,7 +63,10 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
-
+ 
+    /**
+	 * Métodos Get y Set públicos para obtener y devolver valores de los atributos.
+	 */    
 	String getNombre() {
 		return nombre;
 	}
--- deposito/src/cuentas/Main.java
+++ deposito/src/cuentas/Main.java
@@ -1,5 +1,10 @@
 package cuentas;
-
+/**
+ *
+ * @author Carlos Royuela
+ * @version 1.0
+ * @since 09/01/2020
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -9,7 +14,12 @@ public class Main {
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         operativa_cuenta(cuenta1, 0);
     }
-
+    
+    /**
+     * Método que realiza operaciones sobre la cuenta indicada e indica errores si la cantidad es incorrecta.
+     * @param cuenta1 Cuenta sobre la que se opera.
+     * @param cantidad Cantidad con la que se opera.
+     */
 	private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
 		double saldoActual;
 		saldoActual = cuenta1.estado();
